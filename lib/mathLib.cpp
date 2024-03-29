d1af1cd59cfa39654bbde0188590374f6cd704c3
Adam Vesely <adamvesely2003@gmail.com>
2024-03-29 14:20:03 +0100

    Added all current files.

---
+++ lib/mathLib.cpp
@@ -0,0 +1,68 @@
+// Project name: Calculator
+// file: mathLib.h
+// date: 29.03.2024
+// autors:
+// 
+
+#include "include/main.h"
+
+
+// +
+double add(double num1, double num2){
+    return (num1 + num2);
+}
+// -
+double subtract(double num1, double num2){
+    return (num1 - num2);
+}
+// *
+double multiply(double num1, double num2){
+    return (num1 * num2);
+}
+// /
+double divide(double num1, double num2){
+
+    if(num2 == 0) throw std::runtime_error("Cannot divide with 0.");
+    return (num1 / num2);
+}
+// x!
+double factorial(double num){
+
+    if (num < 0 || num > MAX_FAKTORIAL) throw std::runtime_error("Incorrect value.");
+    if (num == 0) return 1;
+
+    return (num * factorial(num - 1));
+}
+// x^
+double exponent(double num, double exp){
+
+    if (exp < 0  || (int(exp) != exp)) throw std::runtime_error("Incorrect value."); // in task exp must natural number
+    if (num == 0 && exp == 0) throw std::runtime_error("0 power of 0 is not defined.");
+    if (exp == 0) return 1;
+
+
+    double number = 1;
+    for (int i = 1; i <= exp; i++) number *= exp;
+    
+    return number;
+}
+// |x|
+double module (double num){
+    if (num < 0) return -num;
+    return num;
+}
+// root(4,2) = 2.0
+double root (double num, double root){
+    if ((root <= 0) || (int(root) != root)) throw std::runtime_error("Incorrect root.");
+    if ((num < 0)   && (int(root) % 2 == 0)) throw std::runtime_error("Incorrect root and value"); // root from a negative number when root is even
+    double guess = num / root; // initial approximation
+    double prev_guess = 0; // to store the previous approximation
+    
+    // until the difference between the current and previous guesses is less than epsilon
+    while (module(guess - prev_guess) > EPSILON) {
+        prev_guess = guess;
+        // update the guess (more .. Newton's method formula for nth root)
+        guess = ((root - 1) * guess + num / exponent(guess, root - 1)) / num;
+    }
+    return guess;
+}
---
+++ src/mainMathFunkceTests.cpp
@@ -0,0 +1,52 @@
+#include "include/main.h"
+#include <gtest/gtest.h>
+
+class TestMainFunkce : public ::testing::Test {
+protected:
+    
+};
+
+TEST_F(TestMainFunkce, AddTest_1) {
+    EXPECT_EQ(13.4, .add(10.3, 3.1));
+}
+
+TEST_F(TestMainFunkce, AddTest_2) {
+    EXPECT_EQ(0.6, .add(-2.0, 1.4));
+}
+
+TEST_F(TestMainFunkce, SubTest_1) {
+    EXPECT_EQ(-35.0, .subtract(2.0, 37.0));
+}
+
+TEST_F(TestMainFunkce, SubTest_2) {
+    EXPECT_EQ(42.01, .subtract(100.23, 58.21));
+}
+
+TEST_F(TestMainFunkce, SubTest_3) {
+    EXPECT_EQ(-106.0, .subtract(-110.0, -4.0));
+}
+
+TEST_F(TestMainFunkce, MultTest_1) {
+    EXPECT_EQ(6.0, .multiply(2.0, 3.0));
+}
+
+TEST_F(TestMainFunkce, MultTest_2) {
+    EXPECT_EQ(-6.0, .multiply(-2.0, 3.0));
+}
+
+TEST_F(TestMainFunkce, MultTest_3) {
+    EXPECT_EQ(7.0, .multiply(-2.0, -3.5));
+}
+
+TEST_F(TestMainFunkce, DivTest_1) {
+    EXPECT_EQ(2.0, .divide(6.0, 3.0));
+}
+
+TEST_F(TestMainFunkce, DivTest_2) {
+    EXPECT_EQ(-2.4, .divide(-6.0, 2.5));
+}
+
+
+TEST_F(TestMainFunkce, DivTest_3) {
+    EXPECT_EQ(DIV_BY_ZERO, .divide(13.31, 0.0));
+}
