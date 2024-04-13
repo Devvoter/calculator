37c3aa6f685ae10dc8b9451e0134fbae9645bbf1
xkremee00 <xkremee00@vutbr.cz>
2024-04-13 23:23:46 +0200

    functional buttons in GUI

--- include/mainwindow.h
+++ include/mainwindow.h
@@ -18,14 +18,41 @@ public:
     MainWindow(QWidget *parent = nullptr);
     ~MainWindow();
 
-private:
-    Ui::MainWindow *ui;
+    //declare variables
+    QString lb_long_string; //string that will be displayed at label lb_string
+    QString lb_number_string; //string that will be displayed at label lb_number
+    bool operation_pressed = false; //true if op button pressed - it will clear out the lb_number label
 
 private slots:
     //displays help in help.html file
-    void on_bt_help_clicked();
+    void on_bt_help_released(); //note - on_buttonName_released() is pre-defined function
+
+    //digit buttons merged
+    void bt_digit_pressed();
+
+    //point
+    void on_bt_point_released();
+
+    //inverse number - upřímně bych to nejradši smazala
+    void on_bt_inverse_released();
+
+    //basic operations buttons (+,-,*,/) merged
+    void bt_basic_op_pressed();
+    
+    void on_bt_modulo_released();
+    void on_bt_abs_released();
+    void on_bt_square_released();
+    void on_bt_exp_released();
+    void on_bt_square_root_released();
+    void on_bt_n_root_released();
+    void on_bt_factorial_released();
+    
+    void on_bt_equal_released();
+    void on_bt_del_released();
+    void on_bt_ac_released();
+
+private:
+    Ui::MainWindow *ui;
 };
-
-
 
 #endif // MAINWINDOW_H
--- src/mainwindow.cpp
+++ src/mainwindow.cpp
@@ -1,14 +1,34 @@
 #include "../include/mainwindow.h"
 #include "../include/ui_mainwindow.h"
 
-#include <QKeyEvent>
-#include <QDesktopServices>
+//#include <QKeyEvent>
+//#include <QDesktopServices>
+//#include <QDebug>
+#include <QObject>
 
 MainWindow::MainWindow(QWidget *parent)
     : QMainWindow(parent)
     , ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
+
+    //conecting digit buttons
+    connect(ui->bt_0, SIGNAL(released()), this, SLOT(bt_digit_pressed()));
+    connect(ui->bt_1, SIGNAL(released()), this, SLOT(bt_digit_pressed()));
+    connect(ui->bt_2, SIGNAL(released()), this, SLOT(bt_digit_pressed()));
+    connect(ui->bt_3, SIGNAL(released()), this, SLOT(bt_digit_pressed()));
+    connect(ui->bt_4, SIGNAL(released()), this, SLOT(bt_digit_pressed()));
+    connect(ui->bt_5, SIGNAL(released()), this, SLOT(bt_digit_pressed()));
+    connect(ui->bt_6, SIGNAL(released()), this, SLOT(bt_digit_pressed()));
+    connect(ui->bt_7, SIGNAL(released()), this, SLOT(bt_digit_pressed()));
+    connect(ui->bt_8, SIGNAL(released()), this, SLOT(bt_digit_pressed()));
+    connect(ui->bt_9, SIGNAL(released()), this, SLOT(bt_digit_pressed()));
+
+    //connecting basic operations buttons
+    connect(ui->bt_plus, SIGNAL(released()), this, SLOT(bt_basic_op_pressed()));
+    connect(ui->bt_minus, SIGNAL(released()), this, SLOT(bt_basic_op_pressed()));
+    connect(ui->bt_multiply, SIGNAL(released()), this, SLOT(bt_basic_op_pressed()));
+    connect(ui->bt_divide, SIGNAL(released()), this, SLOT(bt_basic_op_pressed()));
 }
 
 MainWindow::~MainWindow()
@@ -16,6 +36,99 @@ MainWindow::~MainWindow()
     delete ui;
 }
 
-void MainWindow::on_bt_help_clicked(){
-    QDesktopServices::openUrl(QUrl::fromLocalFile("../calculator/help.html"));
+//this function is just to test the label output
+//to-do: display help
+void MainWindow::on_bt_help_released(){
+    lb_long_string += "help";
+    ui->lb_long->setText(lb_long_string);
+    //QDesktopServices::openUrl(QUrl::fromLocalFile("../calculator/help.html"));
+    //qDebug() << "test_help";
+}
+
+//add digit to string and display it
+void MainWindow::bt_digit_pressed(){
+    //if true, clear lb_number for a new input
+    if (operation_pressed)
+    {
+        lb_number_string = "";
+        operation_pressed = false;
+    }
+    QPushButton * button = (QPushButton*)sender();
+    //QPushButton* button = qobject_cast<QPushButton*>(sender());
+    lb_long_string += button->text();
+    ui->lb_long->setText(lb_long_string);
+    lb_number_string += button->text();
+    ui->lb_number->setText(lb_number_string);
+}
+
+void MainWindow::on_bt_point_released(){
+    lb_long_string += ".";
+    ui->lb_long->setText(lb_long_string);
+}
+
+void MainWindow::on_bt_inverse_released(){
+
+}
+
+void MainWindow::bt_basic_op_pressed(){
+    QPushButton * button = (QPushButton*)sender();
+    //QPushButton* button = qobject_cast<QPushButton*>(sender());
+    lb_long_string += button->text();
+    ui->lb_long->setText(lb_long_string);
+    operation_pressed = true;
+}
+    
+void MainWindow::on_bt_modulo_released(){
+    lb_long_string += "%";
+    ui->lb_long->setText(lb_long_string);
+    operation_pressed = true;
+}
+void MainWindow::on_bt_abs_released(){
+    lb_long_string += "abs";
+    ui->lb_long->setText(lb_long_string);
+    operation_pressed = true;
+}
+void MainWindow::on_bt_square_released(){
+    lb_long_string += "^2";
+    ui->lb_long->setText(lb_long_string);
+    operation_pressed = true;
+}
+void MainWindow::on_bt_exp_released(){
+    lb_long_string += "^n"; //to-do: insert the input exponent
+    ui->lb_long->setText(lb_long_string);
+    operation_pressed = true;
+}
+void MainWindow::on_bt_square_root_released(){
+    lb_long_string += "√";
+    ui->lb_long->setText(lb_long_string);
+    operation_pressed = true;
+}
+void MainWindow::on_bt_n_root_released(){
+    lb_long_string += "ⁿ√";
+    ui->lb_long->setText(lb_long_string);
+    operation_pressed = true;
+}
+void MainWindow::on_bt_factorial_released(){
+    lb_long_string += "!";
+    ui->lb_long->setText(lb_long_string);
+    operation_pressed = true;
+}
+
+void MainWindow::on_bt_equal_released(){
+    lb_long_string += "=";
+    ui->lb_long->setText(lb_long_string);
+}
+void MainWindow::on_bt_del_released(){
+    if (lb_long_string != "")
+    {
+        lb_long_string.chop(1); //removes last character
+    }
+    if (lb_number_string != "")
+    {
+        lb_number_string.chop(1);
+    }
+}
+void MainWindow::on_bt_ac_released(){
+    lb_long_string = "";
+    lb_number_string = "";
 }
