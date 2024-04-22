43b3f206d0922c29275bc70dad9697a80ae65746
Adam Vesely <adamvesely2003@gmail.com>
2024-04-22 22:00:54 +0200

    added Doxyfile for doxygen and fixed some grammatical errors

--- src/include/main.h
+++ src/include/main.h
@@ -1,8 +1,9 @@
-// Project name: Calculator
-// file: main.h
-// date: 26.03.2024
-// autors: 
-// 
+/**
+ * @file main.h
+ * @authors {Denys Pylypenko, Adam Veselý, Eliška Křeménková, Jaroslava Comová}
+ * 
+ * @brief Header file for main.cpp
+*/
 
 #ifndef _MAIN_H_
 #define _MAIN_H_
@@ -18,28 +19,109 @@
 
 #define EPSILON 1e-10
 
-// prototypes for funkcion
-// +
+/**
+ * @brief addition: adds num2 to num1
+ * 
+ * @param[in] num1 first argument of type double
+ * @param[in] num2 second argument of type double
+ * @return double (num1 + num2)
+*/
 double add(double num1, double num2);
-// -
+
+/**
+ * @brief subtraction: subtracts num2 from num1
+ * 
+ * @param[in] num1 first argument of type double
+ * @param[in] num2 second argument of type double
+ * @return double (num1 - num2)
+*/
 double subtract(double num1, double num2);
-// *
+
+/**
+ * @brief multiplication: multiplies num1 by num2
+ * 
+ * @param[in] num1 first argument of type double
+ * @param[in] num2 second argument of type double
+ * @return double (num1 * num2)
+*/
 double multiply(double num1, double num2);
-// /
+
+/**
+ * @brief division: divides num1 by num2
+ * 
+ * @param[in] num1 first argument of type double
+ * @param[in] num2 second argument of type double
+ * @return double (num1 / num2)
+ * @exception throws ERROR_DIVIDE_ZERO if num2 is 0
+*/
 double divide(double num1, double num2);
-// !
+
+/**
+ * @brief factorial: factorializes num
+ * 
+ * @param[in] num number that will be factorialized
+ * @return double (num!) -> (num * (num - 1) * ... * 2 * 1)
+ * @exception throws ERROR_VALUE if num is negative or bigger than MAX_FAKTORIAL
+*/
 double factorial(double num);
-// ^
+
+/**
+ * @brief exponentiation: raises num1 to the power of num2
+ * 
+ * @param[in] num the base number
+ * @param[in] exp the exponent
+ * @return double (num1 ^ num2)
+ * @exception throws ERROR_VALUE if num2 is negative or not an integer
+ * @exception throws ERROR_VALUE if num1 and num2 are both 0
+*/
 double exponent(double num, double exp);
-// |x|
+
+/**
+ * @brief absolute: absolutes num
+ * 
+ * @param[in] num number that will be absoluted
+ * @return double (|num|)
+*/
 double absolute(double num);
-// x --> -x
+
+/**
+ * @brief sign: changes the sign of num
+ * 
+ * @param[in] num number whose sign will be changed
+ * @return double (num * (-1))
+*/
 double sign(double num);
-// x --> 1/x
+
+/**
+ * @brief inversion: inverts num
+ * 
+ * @param[in] num number that will be inverted
+ * @return double (1 / num)
+ * @exception throws ERROR_DIVIDE_ZERO if num is 0
+*/
 double inverse(double num);
-// %
+
+/**
+ * @brief modulo: returns the remainder of num1 divided by num2
+ * 
+ * @param[in] num1 first argument of type double
+ * @param[in] num2 second argument of type double
+ * @return double (num1 % num2)
+*/
 double modulo(double num1, double num2);
-// root(4,2) = 2.0
+
+/**
+ * @brief root: returns the nth root of num
+ * 
+ * @param[in] num number whose root will be returned
+ * @param[in] root number that specifies the type of root (2 for square root, 3 for cube root, n for nth root, etc.)
+ * @return double (num ^ (1 / root))
+ * @exception throws ERROR_VALUE if root is less than or equal to 0
+ * @exception throws ERROR_VALUE if root is not an integer
+ * @exception throws ERROR_VALUE if num is negative and root is an even number (divisible by 2)
+*/
 double root(double num, double root);
 
 #endif
+
+/*** End of file main.h ***/
